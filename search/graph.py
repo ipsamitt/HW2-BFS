c55ec6dde08817c0b1afc1844718fde7b593dc91
ipsamitt <ipsam001@gmail.com>
2025-01-22 22:56:13 -0800

    lots of changes

--- search/graph.py
+++ search/graph.py
@@ -1,4 +1,6 @@
 import networkx as nx
+import matplotlib.pyplot as plt
+
 
 class Graph:
     """
@@ -12,42 +14,53 @@ class Graph:
         """
         self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
 
+    def nodes(self):
+        """
+        Returns the nodes of the graph
+        """
+        return self.graph.nodes()
+    
+    def edges(self):
+        """
+        Returns the edges of the graph
+        """
+        return self.graph.edges()
+    
     def bfs(self, start, end=None):
-  
-
         #If there's no end node input, return a list nodes with the order of BFS traversal
+        visited = []
+        print(start)
+       
 
         if end is None:
             Q = []
-            visited = []
             Q.append(start)
             visited.append(start)
             while len(Q) != 0:
                 node = Q.pop(0)
-                for neighbor in node.neighbors:
+                neighbors = list(self.graph.neighbors(node))
+                for neighbor in neighbors:
                     if neighbor not in visited:
                         visited.append(neighbor)
-                        Q.push(neighbor)
+                        Q.append(neighbor)
 
 
         #If there is an end node input and a path exists, return a list of nodes with the order of the shortest path
         #If there is an end node input and a path does not exist, return None
 
-        if end is None:
+        if end is not None:
+            end = self.graph[end]
             Q = []
-            visited = []
-            Q.append(start)
+            Q.append(self.graph[start])
             visited.append(start)
             while len(Q) != 0:
                 node = Q.pop(0)
-                for neighbor in node.neighbors:
+                for neighbor in list(self.graph.neighbors(node)):
                     if neighbor not in visited:
                         visited.append(neighbor)
-                        Q.push(neighbor)
+                        Q.append(neighbor)
                         if neighbor == end:
                             return visited
         return visited
-
-
-
+    
 
--- test/test_bfs.py
+++ test/test_bfs.py
@@ -1,6 +1,7 @@
 # write tests for bfs
-import pytest
 from search import graph
+import pytest
+import networkx as nx
 
 def test_bfs_traversal():
     """
@@ -10,7 +11,15 @@ def test_bfs_traversal():
     that all nodes are being traversed (ie. returns 
     the right number of nodes, in the right order, etc.)
     """
-    pass
+    new_graph = graph.Graph('data/tiny_network.adjlist')
+    assert(len(new_graph.bfs("Luke Gilbert", end=None))  == 30)
+    ground_truth = nx.bfs_tree(new_graph.graph, source="Luke Gilbert").nodes()
+    assert((new_graph.bfs("Luke Gilbert", end=None)) == (list(ground_truth)))
+
+    assert(len(new_graph.bfs("Atul Butte", end=None))  == 30)
+    ground_truth = nx.bfs_tree(new_graph.graph, source="Atul Butte").nodes()
+    assert((new_graph.bfs("Atul Butte", end=None)) == (list(ground_truth)))
+    
 
 def test_bfs():
     """
@@ -23,4 +32,9 @@ def test_bfs():
     Include an additional test for nodes that are not connected 
     which should return None. 
     """
+    
+    new_graph = graph.Graph('data/citation_network.adjlist')
+
+    
+    
     pass
